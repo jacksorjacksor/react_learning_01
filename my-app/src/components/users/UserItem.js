d2b27994a499bcc6be468ea70b638569e0e410b8
Richard Jackson <jacksonr16@cardiff.ac.uk>
2022-02-21 12:29:42 +0000

    wip and initi

---
+++ my-app/src/components/users/UserItem.js
@@ -0,0 +1,17 @@
+// Largely created from KC: rafcp
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const UserItem = props => {
+    const {login, node_id } = props.user;
+    return (
+        <div>{login} | {node_id}</div>
+    )
+}
+
+// Set types
+UserItem.propTypes = {login:PropTypes.string.isRequired /* KC: ptsr */}
+
+// Export
+export default UserItem 
+
---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Users from './components/users/Users';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Users />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
